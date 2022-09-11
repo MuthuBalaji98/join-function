d1a1793e4bbc42bd069bd77f5e0cd0ef78bb0827
MuthuBalaji98 <muthubalaji1998@gmail.com>
2022-09-11 21:05:40 +0530

    join

---
+++ main.js
@@ -0,0 +1,7 @@
+const fruits = ["Lime","Kiwi", "Orange", "Apple", "Grapes"];
+console.log("original array :", fruits);
+console.log("length of the array is "+fruits.length);
+console.log("method : array.join(and)");
+let result = fruits.join(" and ");
+console.log("change in array : ",result);
+console.log("return: length of array after join() is "+result.length);
